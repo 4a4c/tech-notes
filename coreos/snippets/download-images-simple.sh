7b3777f807c13cd65d6257185be471665982bd9d
Justin Luna <4a4c@users.noreply.github.com>
2017-06-08 08:03:44 -0600

    rev'd to 1353.8.0

--- coreos/snippets/download-images-simple.sh
+++ coreos/snippets/download-images-simple.sh
@@ -1,5 +1,5 @@
 CHANNEL="stable"
-BUILD="1235.6.0"
+BUILD="1353.8.0"
 COREOS_SOURCE="https://${CHANNEL}.release.core-os.net/amd64-usr/${BUILD}"
 FILES="coreos_production_pxe.vmlinuz coreos_production_pxe_image.cpio.gz"
 for FILE in $FILES; do
