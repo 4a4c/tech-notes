e2c222956e61ef6ce7b07d668cfeedae67ec5468
Justin Luna <4a4c@users.noreply.github.com>
2017-10-10 06:50:13 -0600

    version rev

--- coreos/snippets/download-images.sh
+++ coreos/snippets/download-images.sh
@@ -3,7 +3,7 @@
 # Download coreos images for ipxe booting
 
 CHANNEL=stable
-BUILD=899.13.0
+BUILD=1465.8.0
 FILES="coreos_production_pxe_image.cpio.gz coreos_production_pxe_image.cpio.gz.sig coreos_production_pxe.vmlinuz coreos_production_pxe.vmlinuz.sig coreos_production_image.bin.bz2 coreos_production_image.bin.bz2.sig coreos_production_update.bin.bz2 coreos_production_update.bin.bz2.sig coreos_production_vmware_ova.ova coreos_production_vmware_ova.ova.sig"
 LOCAL_DIR="$HOME/Downloads/save/coreos/images"
 
